fd743096828ffcb0d96563c0dfdfee209443e1a0
Xinrui Wang <wang3303@purdue.edu>
2019-04-03 13:00:32 -0700

    add torque controller to be tested

---
+++ franka_example_controllers/src/torque_controller.cpp
@@ -0,0 +1,124 @@
+// Copyright (c) 2017 Franka Emika GmbH
+// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+#include <franka_example_controllers/torque_controller.h>
+
+#include <cmath>
+#include <memory>
+
+#include <controller_interface/controller_base.h>
+#include <franka/robot_state.h>
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+
+#include "pseudo_inversion.h"
+
+namespace franka_example_controllers {
+
+bool TorqueController::init(hardware_interface::RobotHW* robot_hw,
+                                               ros::NodeHandle& node_handle) {
+  sub_torque_ = node_handle.subscribe(
+      "/torque_cmd", 20, &TorqueController::torqueCallback, this,
+      ros::TrcansportHints().reliable().tcpNoDelay());
+
+  std::string arm_id;
+  if (!node_handle.getParam("arm_id", arm_id)) {
+    ROS_ERROR_STREAM("TorqueController: Could not read parameter arm_id");
+    return false;
+  }
+  std::vector<std::string> joint_names;
+  if (!node_handle.getParam("joint_names", joint_names) || joint_names.size() != 7) {
+    ROS_ERROR(
+        "TorqueController: Invalid or no joint_names parameters provided, "
+        "aborting controller init!");
+    return false;
+  }
+
+  auto* model_interface = robot_hw->get<franka_hw::FrankaModelInterface>();
+  if (model_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "TorqueController: Error getting model interface from hardware");
+    return false;
+  }
+  try {
+    model_handle_ = std::make_unique<franka_hw::FrankaModelHandle>(
+        model_interface->getHandle(arm_id + "_model"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "TorqueController: Exception getting model handle from interface: "
+        << ex.what());
+    return false;
+  }
+
+  auto* state_interface = robot_hw->get<franka_hw::FrankaStateInterface>();
+  if (state_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "TorqueController: Error getting state interface from hardware");
+    return false;
+  }
+  try {
+    state_handle_ = std::make_unique<franka_hw::FrankaStateHandle>(
+        state_interface->getHandle(arm_id + "_robot"));
+  } catch (hardware_interface::HardwareInterfaceException& ex) {
+    ROS_ERROR_STREAM(
+        "TorqueController: Exception getting state handle from interface: "
+        << ex.what());
+    return false;
+  }
+
+  auto* effort_joint_interface = robot_hw->get<hardware_interface::EffortJointInterface>();
+  if (effort_joint_interface == nullptr) {
+    ROS_ERROR_STREAM(
+        "TorqueController: Error getting effort joint interface from hardware");
+    return false;
+  }
+  for (size_t i = 0; i < 7; ++i) {
+    try {
+      joint_handles_.push_back(effort_joint_interface->getHandle(joint_names[i]));
+    } catch (const hardware_interface::HardwareInterfaceException& ex) {
+      ROS_ERROR_STREAM(
+          "TorqueController: Exception getting joint handles: " << ex.what());
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void TorqueController::starting(const ros::Time& /*time*/) {
+  tau_d << 0., 0., 0., 0., 0., 0., 0.;
+}
+
+void TorqueController::update(const ros::Time& /*time*/,
+                                                 const ros::Duration& /*period*/) {
+  // get state variables
+  franka::RobotState robot_state = state_handle_->getRobotState();
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_J_d(  // NOLINT (readability-identifier-naming)
+      robot_state.tau_J_d.data());
+  tau_d << saturateTorqueRate(tau_d, tau_J_d);
+  for (size_t i = 0; i < 7; ++i) {
+    joint_handles_[i].setCommand(tau_d(i));
+  }
+}
+
+Eigen::Matrix<double, 7, 1> TorqueController::saturateTorqueRate(
+    const Eigen::Matrix<double, 7, 1>& tau_d_calculated,
+    const Eigen::Matrix<double, 7, 1>& tau_J_d) {
+  Eigen::Matrix<double, 7, 1> tau_d_saturated{};
+  for (size_t i = 0; i < 7; i++) {
+    double difference = tau_d_calculated[i] - tau_J_d[i];
+    tau_d_saturated[i] =
+        tau_J_d[i] + std::max(std::min(difference, delta_tau_max_), -delta_tau_max_);
+  }
+  return tau_d_saturated;
+}
+
+void TorqueController::torqueCallback(
+    const franka_msgs::Torque& msg) {
+  for (int i = 0; i < 7; ++i)
+    tau_d(i)= msg.torque[i]
+}
+
+}  // namespace franka_example_controllers
+
+PLUGINLIB_EXPORT_CLASS(franka_example_controllers::TorqueController,
+                       controller_interface::ControllerBase)
